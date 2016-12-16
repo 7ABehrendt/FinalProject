494e738ccb603d490e9bb5089e6093e3fbf0d903
7ABehrendt <7abehrendt@stpatrick.org>
2016-12-16 10:51:55 -0600

    finished

--- cart.js
+++ cart.js
@@ -1,30 +1,33 @@
 class Cart{
   //What is the first part of every class? Type it below.
-constructor(i,q){
-  this.itemList = i;
-  this.itemquality = q;
-}
-
-
-  //Type the instance functions below this comment.
-addItem(i,q){
-  this.itemList.push(i);
-  this.itemquality.push(q);
-}
-totalCart(){
-  let total = 0;
-  for(a=0;a<this.itemlist.length;a++){
-    total += this.itemList.[a].price*this.itemquality[a];
+  constructor(itemList,itemQuantity){
+    this.itemList = itemList;
+    this.itemQuantity = itemQuantity;
   }
-  return total;
-}
-subCart(days){
-  let subCart = new Cart([],[]);
-  for(b=0;b<this.itemList.length;a++){
-    if(this.itemList[b].shipping == days){
-      subCart.push([b]);
+  addItem(i,q){
+   this.itemList.push(i);
+   this.itemQuantity.push(q);
+ }
+ totalCart(){
+   let total = 0
+   for(let t = 0; t<this.itemList.length; t++){
+      let item = (this.itemList[t].price)*this.itemQuantity[t];
+      total = total + item;
     }
-  }
-  return subCart
-}
+   return total;
+ }
+ subcart(days){
+   let subcart = new Cart([],[]);
+   for(let s=0;s<this.itemList.length;s++){
+     if(this.itemList[t].shipping == days){
+       subcart.push(this.itemList[s]);
+       subcart.push(this.itemQuantity[s]);
+     }
+     else{
+       console.log('Your search returned no results.');
+     }
+   }
+   console.log(subcart);
+   return subcart
+ }
 }
